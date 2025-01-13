5aa063057ba7372925f1986cc81f585df8ad778f
Pawan Kumar Gupta <pawankumargupta365@gmail.com>
2025-01-13 19:38:28 +0530

    Vercel Analytics imported

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,14 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import CoinContextProvider from './context/CoinContext.jsx'
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
     <CoinContextProvider>
     <App />
+    <Analytics />
     </CoinContextProvider>
     </BrowserRouter>
   </StrictMode>
